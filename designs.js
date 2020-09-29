00a6d677d9fb501f63f264b9e30212fa4720a640
Zlawati <70705625+Zlawati@users.noreply.github.com>
2020-09-29 18:54:05 +0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  // for storing the height , width , canvas values
+  const height = document.getElementById('#inputHeight').value;
+  const width = docuemnt.getElementById('#inputWidth').value;
+  const canvas = docuemnt.getElementById('#colorPicker').value;
 
-// Your code goes here!
+  // using while loop to delete the rows
+  while (canvas.rows.length > 0) {
+    canvas.DeleteRow(0);
+  }
 
+  // using for loops to draw the rows and nesting for the cells
+  for(let i = 0; i < height; i++) {
+    let row = canvas.insertRow(i);
+
+    for(let w = 0; w < width; w++) {
+      let cell = row.insertCell(w);
+
+      cell.addEventListener('click', function(evt){
+        event.target.style.backgroundColor = document.getElementById("#colorPicker")
+      });
+    }
+  }
 }
+
+docuemnt.getElementById('#sizePicker').addEventListener('submit', function(event){
+  event.preventDefault();
+  makeGrid();
+});
